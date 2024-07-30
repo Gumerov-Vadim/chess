4e561951a22b3b349b25acf79326b6961bb43c3e
Gumerov-Vadim <95773619+Gumerov-Vadim@users.noreply.github.com>
2024-07-30 06:36:50 +0500

    added some constructors for figures

--- script.js
+++ script.js
@@ -10,17 +10,138 @@ const LITERAS = {
 }
 
 const FIGURES = {
-    PAWN:"",
-    ROOK:"",
-    KNIGHT:"",
-    BISHOP:"",
-    QUEEN:"",
-    KING:"",
-    EMPTY:"",
+    PAWN:"pawn",
+    ROOK:"rook",
+    KNIGHT:"knight",
+    BISHOP:"bishop",
+    QUEEN:"queen",
+    KING:"king",
+    EMPTY:"empty",
 }
+
+const FIGURES_IMAGES_SRC = {
+    BLACK: {
+    BLACK_PAWN:"./images/black_pawn.png",
+    BLACK_ROOK:"./images/black_rook.png",
+    BLACK_KNIGHT:"./images/black_knight.png",
+    BLACK_BISHOP:"./images/black_bishop.png",
+    BLACK_QUEEN:"./images/black_queen.png",
+    BLACK_KING:"./images/black_king.png",
+        },
+    WHITE: {
+    WHITE_PAWN:"./images/white_pawn.png",
+    WHITE_ROOK:"./images/white_rook.png",
+    WHITE_KNIGHT:"./images/white_knight.png",
+    WHITE_BISHOP:"./images/white_bishop.png",
+    WHITE_QUEEN:"./images/white_queen.png",
+    WHITE_KING:"./images/white_king.png",
+        },
+}
+
+const COLORS = {
+    BLACK: "black",
+    WHITE: "white",
+}
+
+function Pawn(color){
+    this.name = FIGURES.PAWN;
+    this.color = color;
+    
+    if(color === COLORS.BLACK){
+        this.image_src = FIGURES_IMAGES_SRC.BLACK.BLACK_PAWN;
+    } else {
+        this.image_src = FIGURES_IMAGES_SRC.WHITE.WHITE_PAWN;
+    }
+}
+function Rook(color){
+    this.name = FIGURES.ROOK;
+    this.color = color;
+    
+    if(color === COLORS.BLACK){
+        this.image_src = FIGURES_IMAGES_SRC.BLACK.BLACK_ROOK;
+    } else {
+        this.image_src = FIGURES_IMAGES_SRC.WHITE.WHITE_ROOK;
+    }
+}
+function Bishop(color){
+    this.name = FIGURES.PAWN;
+    this.color = color;
+    
+    if(color === COLORS.BLACK){
+        this.image_src = FIGURES_IMAGES_SRC.BLACK.BLACK_BISHOP;
+    } else {
+        this.image_src = FIGURES_IMAGES_SRC.WHITE.WHITE_BISHOP;
+    }
+}
+function Knight(color){
+    this.name = FIGURES.PAWN;
+    this.color = color;
+    
+    if(color === COLORS.BLACK){
+        this.image_src = FIGURES_IMAGES_SRC.BLACK.BLACK_KNIGHT;
+    } else {
+        this.image_src = FIGURES_IMAGES_SRC.WHITE.WHITE_KNIGHT;
+    }
+}
+
 function ChessboardSquare(litera,number){
     this.litera = litera;
     this.number = number;
+    this.figure = {};
+    
+    switch(litera){
+        case "b":
+            this.figure = FIGURES.WHITE_PAWN;
+            break;
+        case "g":
+            this.figure = FIGURES.BLACK_PAWN;
+            break;
+        case "a":
+            switch(number){
+                case 1:
+                case 8:
+                    this.figure = FIGURES.WHITE_ROOK;
+                    break;
+                case 2:
+                case 7:
+                    this.figure = FIGURES.WHITE_KNIGHT;
+                    break;
+                case 3:
+                case 6:
+                    this.figure = FIGURES.WHITE_BISHOP;
+                    break;
+                case 4:
+                    this.figure = FIGURES.WHITE_KING;
+                    break;
+                case 5:
+                    this.figure = FIGURES.WHITE_QUEEN;
+                    break;
+            }
+            break;
+        case "g":
+            switch(number){
+                case 1:
+                case 8:
+                    this.figure = FIGURES.BLACK_ROOK;
+                    break;
+                case 2:
+                case 7:
+                    this.figure = FIGURES.BLACK_KNIGHT;
+                    break;
+                case 3:
+                case 6:
+                    this.figure = FIGURES.BLACK_BISHOP;
+                    break;
+                case 4:
+                    this.figure = FIGURES.BLACK_KING;
+                    break;
+                case 5:
+                    this.figure = FIGURES.BLACK_QUEEN;
+                    break;
+            }
+            break;
+    }
+    //to do
 }
 const chessFieldModel= {
 
